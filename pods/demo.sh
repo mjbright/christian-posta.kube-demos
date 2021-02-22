ded55a75154bf473b058d1694e9eb8eaf67be028
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-02-22 21:45:53 +0100

    Changed to '-n demos'

--- pods/demo.sh
+++ pods/demo.sh
@@ -6,20 +6,20 @@ kubectl get namespace demos ||
     die "No namespace 'demos' - run setup.sh first"
 
 desc "There are no running pods"
-run "kubectl --namespace=demos get pods"
+run "kubectl -n demos get pods"
 
 desc "Create a pod"
 run "cat $(relative pod.yaml)"
-run "kubectl --namespace=demos create -f $(relative pod.yaml)"
+run "kubectl -n demos create -f $(relative pod.yaml)"
 
 desc "Hey look, a pod!"
-run "kubectl --namespace=demos get pods"
+run "kubectl -n demos get pods"
 
 desc "Get the pod's IP"
-run "kubectl --namespace=demos get pod pods-demo-pod -o custom-columns=.NAME:status.podIP --no-headers"
+run "kubectl -n demos get pod pods-demo-pod -o custom-columns=.NAME:status.podIP --no-headers"
 
 trap "" SIGINT
-IP=$( kubectl --namespace=demos get pod pods-demo-pod -o custom-columns=.NAME:status.podIP --no-headers )
+IP=$( kubectl -n demos get pod pods-demo-pod -o custom-columns=.NAME:status.podIP --no-headers )
 
 desc "poke the pod"
 node_run "for i in \$(seq 1 10); do \\
@@ -29,4 +29,4 @@ node_run "for i in \$(seq 1 10); do \\
     "
 
 desc "Let's cleanup and delete that pod"
-run "kubectl --namespace=demos delete pod pods-demo-pod"
+run "kubectl -n demos delete pod pods-demo-pod"
