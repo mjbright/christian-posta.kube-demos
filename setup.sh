604ee5358ab6973781e296e987ef9d08a4dc3be2
Michael Bright <1880109+mjbright@users.noreply.github.com>
2021-02-22 20:29:01 +0100

    Initial commit of forked repo ...
    - converting to Kubernetes only
    
    - Updating for Kubernetes 1.20+

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 
 . $(dirname ${BASH_SOURCE})/util.sh
 
+kubectl create -f demo-namespace.yaml
 
-oc new-project demos
-oc policy add-role-to-user edit system:serviceaccount:demos:deployer
+#oc new-project demos
+#oc policy add-role-to-user edit system:serviceaccount:demos:deployer
+#
+#oc policy add-role-to-user edit system:serviceaccount:demos:default
+#oc adm policy add-scc-to-user anyuid system:serviceaccount:demos:default
+#oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
 
-oc policy add-role-to-user edit system:serviceaccount:demos:default
-oc adm policy add-scc-to-user anyuid system:serviceaccount:demos:default
-oc policy add-role-to-user view system:serviceaccount:$(oc project -q):default -n $(oc project -q)
